0f674eb60d6597be6d166245390e7875948d3af3
Gerardo <gherrer1@binghamton.edu>
2018-08-13 21:50:01 -0400

    added proptypes to LanguageButton

--- app/LanguageButton.js
+++ app/LanguageButton.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 
 class LanguageButton extends React.Component {
   render() {
@@ -6,6 +7,10 @@ class LanguageButton extends React.Component {
       <button className={`${this.props.active ? "active" : false} lang-btn`} >{this.props.language}</button>
     );
   }
+}
+LanguageButton.propTypes = {
+  language: PropTypes.string.isRequired,
+  active: PropTypes.bool
 }
 
 module.exports = LanguageButton;
