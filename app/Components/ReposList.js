e477ca181a7243d21e5ffbe5304a8e2f417abf61
Gerardo <gherrer1@binghamton.edu>
2018-08-15 12:30:50 -0400

    Displaying Repo data when language is selected. Need to do last 20% which is styling and whatnot.

--- app/Components/ReposList.js
+++ app/Components/ReposList.js
@@ -1,7 +1,25 @@
 var React = require('react');
 
+function RepoItem(props) {
+  return (
+    <li>
+      <p>#{props.rank}</p>
+      <img src={props.repo.owner.avatar_url} className="repo-avatar" />
+      <p>{props.repo.name}</p>
+      <p>@{props.repo.owner.login}</p>
+      <p>{props.repo.stargazers_count} stars</p>
+    </li>
+  );
+}
+
 function ReposList(props) {
-  return (<h1>Hey</h1>);
+  return (
+    <ul className="repo-list" >
+      {
+        props.repos.map((repo, index) => (<RepoItem key={repo.id} rank={index+1} repo={repo} />))
+      }
+    </ul>
+  );
 }
 
 module.exports = ReposList;
