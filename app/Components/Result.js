c1a995cfc518be574e5185875c1846132a0fec02
Gerardo <gherrer1@binghamton.edu>
2018-08-19 00:45:53 -0400

    once again, leveraged the now more reusable PlayerView component by passing it children

--- app/Components/Result.js
+++ app/Components/Result.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
+const PlayerView = require('./PlayerView');
 
 function Result({playerData}) {
     let p = playerData;
@@ -8,22 +9,18 @@ function Result({playerData}) {
         <div className="column">
             <h2>{p.won ? 'Winner' : 'Loser'}</h2>
             <h2>Score: {p.score}</h2>
-            <img
-                className="usr-img"
-                alt={`Avatar for ${p.profile.login}`}
-                src={p.profile.avatar_url}
-            />
-            <h2>@{p.profile.login}</h2>
-            <div className="not-centered">
-                <ul>
-                    <li>{p.profile.name}</li>
-                    <li>{p.profile.location}</li>
-                    <li>{p.profile.bio}</li>
-                    <li>Followers: {p.profile.followers}</li>
-                    <li>Following: {p.profile.following}</li>
-                    <li>Public Repos: {p.profile.public_repos}</li>
-                </ul>
-            </div>
+            <PlayerView playerData={p.profile}>
+                <div className="not-centered">
+                    <ul>
+                        <li>{p.profile.name}</li>
+                        <li>{p.profile.location}</li>
+                        <li>{p.profile.bio}</li>
+                        <li>Followers: {p.profile.followers}</li>
+                        <li>Following: {p.profile.following}</li>
+                        <li>Public Repos: {p.profile.public_repos}</li>
+                    </ul>
+                </div>
+            </PlayerView>
         </div>
     );
 };
